e056561f313a9af8507f58d156d0ef0a64211ce4
onquit <onquit89@gmail.com>
2024-04-03 18:21:05 +0800

    added index

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity 6 Onquit</title>
+</head>
+<body style="display:flex; justify-content:center; margin-top:30%; background:black">
+    <div style="width:200px; height:100px; box-shadow:0px 0px 10px orange, 0px 0px 10px orange inset; padding:10px; color: orange">
+        <hl >My Activity 6</h1>
+        <ul >
+            <li><a href="/posts/1">Post 1</a></li>
+            <li><a href="/posts/2">Post 2</a></li>
+        </ul>
+    
+    </div>  
+</body>
+</html>
